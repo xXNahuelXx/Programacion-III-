41d37237fdc89a98744f76358722d1c622b401d3
Nahuel <nahuugh98@gmail.com>
2023-03-27 18:57:07 -0300

    Terminado

---
+++ Ejercicios/Clase 1/App N3/Sumar_dos_numeros_II.php
@@ -0,0 +1,13 @@
+<?php
+
+/*Partiendo del ejercicio anterior, modificar la salida por pantalla para que se visualice el valor de 
+la variable $x, el valor de la variable $y y el resultado final en líneas distintas (recordar que el 
+salto de línea en HTML es la etiqueta <br/>). */
+
+$x = -3;
+$y = 15;
+$result = $x + $y;
+
+echo $x . "<br>" . $y . "<br>" . "resultado: ",$result;
+
+?>
