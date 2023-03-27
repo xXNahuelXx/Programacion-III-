0e1c9ca59784709284f197d1595c8d4d4d79f56d
Nahuel <nahuugh98@gmail.com>
2023-03-27 15:45:32 -0300

    Terminado

---
+++ Ejercicios/Clase 1/App N1/Mostrar_Variables.php
@@ -0,0 +1,12 @@
+<?php
+
+/*Realizar un programa que guarde su nombre en $nombre y su apellido en $apellido. Luego 
+mostrar el contenido de las variables con el siguiente formato: Pérez, Juan. Utilizar el operador 
+de concatenación.*/
+
+$nombre = "Nahuel";
+$apellido = "Ghigliotti";
+
+echo $apellido . ", " . $nombre;
+
+?>
