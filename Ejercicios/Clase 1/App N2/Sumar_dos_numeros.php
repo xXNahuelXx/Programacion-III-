bc86166a6f75fe18af618dae1e6b4b63572a2b61
Nahuel <nahuugh98@gmail.com>
2023-03-27 15:56:45 -0300

    Terminado

---
+++ Ejercicios/Clase 1/App N2/Sumar_dos_numeros.php
@@ -0,0 +1,11 @@
+<?php
+
+/*Hacer un programa en PHP que sume el contenido de dos variables $x = -3 y $y = 15. Mostrar
+el resultado final.*/
+
+$x = -3;
+$y = 15;
+$result = $x + $y;
+
+echo $result;
+?>
