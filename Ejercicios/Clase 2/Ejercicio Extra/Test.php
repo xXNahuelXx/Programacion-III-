2471bdd5aabc3b878f9df025661ab1fba558d804
Nahuel <nahuugh98@gmail.com>
2023-04-07 21:11:47 -0300

    Terminado, agrego pruebas de clase nueva

--- Ejercicios/Clase 2/Ejercicio Extra/Test.php
+++ Ejercicios/Clase 2/Ejercicio Extra/Test.php
@@ -1,8 +1,11 @@
 <?php
 
+
 use LittleAnimals\Pet;
+use Business\Nursery;
 
 require_once "./Clases/Pet.php";
+require_once "./Clases/Nursery.php";
 
 $petOne = new Pet("Sindy", "Poodle");
 $petTwo = new Pet("Sindy", "Yorkshire");
@@ -23,3 +26,11 @@ echo "<br>";
 
 echo $petThree->equals($petFour) ? "Mascota 3 y 4 SON iguales<br>" : "Mascota 3 y 4 NO son iguales<br>";
 echo $petOne->equals($petThree) ? "Mascota 1 y 3 SON iguales<br>" : "Mascota 1 y 3 NO son iguales<br>";
+
+$nursery = new Nursery("La guarderia de Pancho");
+$nursery->Add($petOne);
+$nursery->Add($petTwo);
+$nursery->Add($petThree);
+$nursery->Add($petFour);
+
+echo $nursery->__toString();
